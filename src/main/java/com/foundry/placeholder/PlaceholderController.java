94a394158038703323e38c1cfb969a9303e494d0
Conor <driscollconor3@gmail.com>
2021-11-30 17:33:49 -0500

    Project Finished

---
+++ src/main/java/com/foundry/placeholder/PlaceholderController.java
@@ -0,0 +1,55 @@
+package com.foundry.placeholder;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import com.foundry.placeholder.Models.*;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/api")
+public class PlaceholderController {
+    private final String URL = "https://jsonplaceholder.typicode.com/";
+
+
+    @GetMapping("/users/{id}")
+    public User getUserID(@PathVariable String id, RestTemplate restTemplate){
+        return restTemplate.getForObject(URL + "users/"+ id, User.class);
+    }
+
+    @GetMapping("/users")
+    public User[] getUserRange(RestTemplate restTemplate, @RequestParam(name = "start", required = false) String start, @RequestParam(name = "end", required = false) String end){
+        if(start == null){
+            return restTemplate.getForObject(URL + "users/", User[].class);
+        }
+        else{
+            ArrayList<User> users = new ArrayList<>();
+            for (int i = Integer.parseInt(start); i <= Integer.parseInt(end); i++) {
+                users.add(restTemplate.getForObject(URL + "users/" + i, User.class));
+            }
+            return users.toArray(new User[0]);
+        }
+    }
+
+
+    @GetMapping("/photos/{id}")
+    public Photo getPhotoID(@PathVariable String id, RestTemplate restTemplate){
+        return restTemplate.getForObject(URL + "photos/"+ id, Photo.class);
+
+    }
+
+    @GetMapping("/photos")
+    public Photo[] getPhotoRange(RestTemplate restTemplate, @RequestParam(name = "start", required = false) String start, @RequestParam(name = "end", required = false) String end){
+        if(start == null){
+            return restTemplate.getForObject(URL + "photos/", Photo[].class);
+        }
+        else{
+            ArrayList<Photo> photos = new ArrayList<>();
+            for (int i = Integer.parseInt(start); i <= Integer.parseInt(end); i++) {
+                photos.add(restTemplate.getForObject(URL + "photos/" + i, Photo.class));
+            }
+            return photos.toArray(new Photo[0]);
+        }
+
+    }
+}
